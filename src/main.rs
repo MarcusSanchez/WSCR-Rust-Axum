d717d143eee02150a405008b078fd6ae6f59e203
Marcus Sanchez <marcussanchez031@gmail.com>
2023-09-07 00:24:31 -0400

    set up for railway

--- src/main.rs
+++ src/main.rs
@@ -5,15 +5,22 @@ mod routes;
 
 use axum::{Router};
 use runhub::run_hub;
+use std::env;
 
 #[tokio::main]
 async fn main() {
+    let port = match env::var("PORT") {
+        Ok(val) => val,
+        Err(_) => "3000".to_string(),
+    };
+    let addr = format!("0.0.0.0:{}", port);
+
     let mut app = Router::new();
     app = routes::start_routing(app);
     tokio::spawn(run_hub());
 
     println!("Server running on: http://localhost:3000");
-    axum::Server::bind(&"0.0.0.0:3000".parse().unwrap())
+    axum::Server::bind(&addr.parse().unwrap())
         .serve(app.into_make_service())
         .await
         .unwrap();
